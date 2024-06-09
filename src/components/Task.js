e1974ca4c8bf5136bbe04865e2308c0c6a8287ca
Josefine Brorson <josefinebrorson@MacBook-Pro-som-tillhor-Josefine.local>
2024-06-09 22:56:15 +0200

    fix: handle delete from list

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,47 +1,15 @@
-// import React from 'react';
-// import { DigiFormCheckbox, DigiButton } from '@digi/arbetsformedlingen-react';
-// import { ButtonVariation } from '@digi/arbetsformedlingen';
-
-// const Task = ({ id, title, onDelete, onEdit }) => {
-//     const handleEditClick = () => {
-//         const newTitle = prompt('Ange ny titel:', title);
-//         if (newTitle !== null) {
-//             onEdit(id, newTitle);
-//         }
-//     };
-
-//     const handleDeleteClick = () => {
-//         if (window.confirm('Är du säker på att du vill ta bort uppgiften?')) {
-//             onDelete(id);
-//         }
-//     };
-
-//     return (
-//         <div>
-//             <DigiFormCheckbox afLabel={title} />
-//             <DigiButton afVariation={ButtonVariation.SECONDARY} onClick={handleEditClick}>Redigera</DigiButton>
-//             <DigiButton onClick={handleDeleteClick}>Ta bort</DigiButton>
-//         </div>
-//     );
-// }
-
-// export default Task;
-
-// Task.js
 import React from 'react';
+import { DigiFormCheckbox, DigiButton } from '@digi/arbetsformedlingen-react';
+import { ButtonVariation } from '@digi/arbetsformedlingen';
 
 const Task = ({ task, onToggleComplete, onDelete }) => {
     return (
         <div>
-            <input
-                type="checkbox"
-                checked={task.completed}
-                onChange={() => onToggleComplete(task.id)}
-            />
-            <span style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>
-                {task.title}
-            </span>
-            <button onClick={() => onDelete(task.id)}>Ta bort</button>
+            <div>
+                <DigiFormCheckbox afLabel={task.title} type="checkbox" checked={task.completed} onChange={() => onToggleComplete(task.id)}/>
+                <DigiButton afVariation={ButtonVariation.SECONDARY}>Redigera</DigiButton>
+                <DigiButton onClick={() => onDelete(task.id)}>Ta bort</DigiButton>
+            </div>
         </div>
     );
 }
